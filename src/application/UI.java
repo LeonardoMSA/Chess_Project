962cc5c4d2d743887e3d745ae46572d0d2effd67
LeonardoMSA <leonardomsazevedo@gmail.com>
2023-06-19 21:13:27 -0300

    Board shows possible moves of piece
    Fixed problem with the background approach that was shown in the course, switched it so that it marks the possible squares with an 'x'and marks the threatened pieces as red

--- src/application/UI.java
+++ src/application/UI.java
@@ -82,19 +82,26 @@ public class UI {
 
 	private static void printPiece(ChessPiece piece, boolean background) {
 		
-		/*if(background) {
-			System.out.print(ANSI_GREEN_BACKGROUND);
-		}*/
-    	if (piece == null) {
+		if(piece == null && background) {
+			System.out.print('x');
+		}
+		else if (piece == null) {
             System.out.print("-");
         }
         else {
             if (piece.getColor() == Color.WHITE) {
-            	
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            	if(background) {
+            		System.out.print(ANSI_RED + piece + ANSI_RESET);
+            	}else {            		
+            		System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            	}
             }
             else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            	if(background) {
+            		System.out.print(ANSI_RED + piece + ANSI_RESET);
+            	}else {            		
+            		System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            	}
             }
         }
         System.out.print(" ");
